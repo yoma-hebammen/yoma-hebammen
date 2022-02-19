5d69d13cd13fe6441cb0be935ad41a0542ff8c44
jonixis <joni@stahlfamily.ch>
2022-02-19 17:15:43 +0100

    Fix menu scroll

--- src/header.js
+++ src/header.js
@@ -3,6 +3,7 @@ const toggleNavbar = () => {
   const navbarElementId = "navbar";
   const navbarBurgerElementId = "navbar-burger";
 
+  const bodyElements = document.getElementsByTagName("body");
   const navbarElement = document.getElementById(navbarElementId);
   const navbarBurgerElement = document.getElementById(navbarBurgerElementId);
   const isActive = navbarElement.classList.contains(isActiveClassName);
@@ -10,9 +11,11 @@ const toggleNavbar = () => {
   if (!isActive) {
     navbarElement.classList.add(isActiveClassName);
     navbarBurgerElement.classList.add(isActiveClassName);
+    bodyElements.item(0).style.overflow = "hidden";
   } else {
     navbarElement.classList.remove(isActiveClassName);
     navbarBurgerElement.classList.remove(isActiveClassName);
+    bodyElements.item(0).style.removeProperty("overflow");
   }
 };
 
