5d270a088b7d7b055c6e848d4b313fd4e26f8c34
jonixis <joni@stahlfamily.ch>
2023-03-05 17:31:14 +0100

    Update gitignore

---
+++ .netlify/functions/api/cloudinary.js
@@ -0,0 +1,39 @@
+const serverless = require('serverless-http')
+const express, { Router } = require('express')
+const { isAuthorized } = require('@tinacms/auth')
+const { createMediaHandler } = require('next-tinacms-cloudinary/dist/handlers')
+
+const app = express()
+
+const router = Router()
+
+const handler = createMediaHandler({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  authorized: async (req, _res) => {
+    try {
+      if (process.env.NODE_ENV == 'development') {
+        return true
+      }
+
+      const user = await isAuthorized(req)
+
+      return user && user.verified
+    } catch (e) {
+      console.error(e)
+      return false
+    }
+  },
+})
+
+router.get('/cloudinary/media', handler)
+
+router.post('/cloudinary/media', handler)
+
+router.delete('/cloudinary/:media', (req, res) => {
+  req.query.media = ['media', req.params.media]
+  return handler(req, res)
+})
+
+app.use('/api/', router)
